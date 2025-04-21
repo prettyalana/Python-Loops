d1737eee34185e9ef8f0ed4b888d94837989575b
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-04-21 10:51:03 -0500

    Add bonus assignment code - Define a custom range between 1 and 10 - Implement a while loop to kee prompting the user for a whole number in the range - Add error handling for non integer inputs using try/except

--- bonus.py
+++ bonus.py
@@ -5,3 +5,20 @@ max = 20
 while counter < max :
     counter += 2
     print(counter)
+ 
+# Range    
+custom_range = range(1,11)
+
+while True :
+    try:
+        number = int(input("Choose a number between 1 and 10: "))
+
+        if number not in custom_range :
+            print("You have chosen a number outside of the range.")
+        else :
+            print(f"You have chosen number: {number}")
+            break
+    except ValueError:
+        print("Enter a whole number.")
+
+
